06e7562cad2fc28dca8a57944555cffc53736c34
johndward01 <johndward01@gmail.com>
2021-10-07 10:56:20 -0500

    update

--- Method_Overloading_Demo/Method_Overloading_Demo/Program.cs
+++ Method_Overloading_Demo/Method_Overloading_Demo/Program.cs
@@ -7,44 +7,64 @@ namespace Method_Overloading_Demo
     {
         static void Main(string[] args)
         {
-            //Methods can have the same name as long as there SIGNATURES are different
-
-            //Method Overloading allows methods of the same name to happily co-exist in the same program, providing their parameters differ in 
-            //  1. Number or
-            //  2. Data Type or
-            //  3. Number AND Data Type
-
-            // NUMBER of parameters differ (data type the same)
-            Add(1, 2); 
-            Add(1, 2, 3); 
-
-            // DATA TYPE of parameters differ (number of parameters the same)
-            Add(0, 1);
-            Add(.1, .2);
-
-            // NUMBER & DATA TYPES 
-            Add(.001, .002, .003);
-            Add(1, 2, 3, 4);
+            //Subtract(1000, 2);
+            Console.WriteLine(true);
+            Add(1, 1, 2);
+            Add(1.99, 2.99);
         }
 
-        public static void Add(int x, int y)
+
+        public static int Add(int a, int b)
         {
-            Console.WriteLine($"The sum of {x} + {y} = {x + y}");
+            return a + b;
         }
 
-        public static void Add(int w, int x, int y,int z)
+        public static double Add(double a, double b)
         {
-            Console.WriteLine($"The sum of {x} + {y} + {z} = {w + x + y + z}");
+            return a + b;
         }
 
-        public static void Add(double x, double y)
+        public static int Add(int a, int b, int c)
         {
-            Console.WriteLine($"The sum of {x} + {y} = {x + y}");
+            return a + b + c;
         }
 
-        public static void Add( double x, double y, double z)
-        {
-            Console.WriteLine($"The sum of {x} + {y} + {z} = {x + y + z}");
-        }
+        //public static int Subtract(int x, int y)
+        //{
+        //    return x - y;
+        //}
+
+
+
+
+        //public static decimal Add(float x, decimal y)
+        //{
+        //    return (int)x + y;
+        //}
+
+        //public static double Add(double x, double y)
+        //{
+        //    return x + y;
+        //}
+
+        //public static decim
+
+
+
+
+
+        //public static int Add(params int[] x)
+        //{
+        //    int answer = 0;
+
+        //    foreach (var num in x)
+        //    {
+        //        answer += num;
+        //    }
+
+        //    return answer;
+        //}
+
+        
     }
 }
---
+++ Method_Overloading_Exercise/Method_Overloading_Exercise/ConsoleWindowManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Method_Overloading_Exercise
+{
+    public class ConsoleWindowManager
+    {
+        public string Title { get; set; }
+
+        public void PrintSomething()
+        {
+            Console.WriteLine("Something");
+        }
+
+        public ConsoleWindowManager(string firstName)
+        {
+                
+        }
+
+    }
+}
